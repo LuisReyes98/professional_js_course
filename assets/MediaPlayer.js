00448ed39bff1cb6f35f2bc1df52537dad979754
LuisReyes98 <luisrogelio98@gmail.com>
2019-08-29 12:50:16 -0400

    prototypes calls and this

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,21 +1,41 @@
 function MediaPlayer(config) {
-    this.media = config.el
+    this.media = config.el;
+    this.plugins = config.plugins || [] ;
+
+    this._initPlugins();
 }
+MediaPlayer.prototype._initPlugins = function () {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+};
 
 MediaPlayer.prototype.play = function () {
     this.media.play();
-}
+};
 
 MediaPlayer.prototype.pause = function () {
     this.media.pause();
-}
+};
 
-MediaPlayer.prototype.toggle = function () {
+MediaPlayer.prototype.togglePlay = function () {
     if (this.media.paused) {
         this.media.play();
     } else {
         this.media.pause();
     }
+};
+
+MediaPlayer.prototype.mute = function () {
+    this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+    this.media.muted = false;
+};
+
+MediaPlayer.prototype.toggleMute = function () {
+    this.media.muted = !this.media.muted;
 }
 
 export default MediaPlayer;
